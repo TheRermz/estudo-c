400c6903e543b8609e7a23a0d84f66da25d38d72
Murilo Fischer <43183795+TheRermz@users.noreply.github.com>
2024-02-26 14:54:02 -0300

    exercicio 6 incompleto

---
+++ 00-exercicios/ex_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * Fazer um programa que leia três valores com ponto flutuante de dupla
+ * precisão: A, B e C. Em seguida, calcule e
+ * mostre:
+ * a) a área do triângulo retângulo que tem A por base e C por altura. || bh/2
+ * b) a área do círculo de raio C. (pi = 3.14159) || pi.r²
+ * c) a área do trapézio que tem A e B por bases e C por altura.|| (a+b/2).h
+ * d) a área do quadrado que tem lado ||B. b²
+ * e) a área do retângulo que tem lados A e B.|| a.b
+ */
+
+int main()
+{
+
+    return 0;
+}
