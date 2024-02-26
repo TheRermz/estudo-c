12e3879fb9de8abcd36a6b4407d7731940c47727
Murilo Fischer <mufischer@hotmail.com>
2024-02-25 22:37:16 -0300

    basico

---
+++ 1-basico/basics-io.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //allows to use fuctions from this library
+
+int main()
+{
+    printf("olar\n");
+    int x = 50;
+    printf("%s World\n", "Hello");
+    printf("The value of x is %d\n", x);
+    printf("Give me a radius:");
+    int radius;
+    scanf("%d", &radius); // address of operator (pointer)
+    printf("You chose dea... the value %d\n", radius);
+    return 0;
+}
