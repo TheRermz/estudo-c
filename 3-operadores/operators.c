f981d107e04c5ba98ece49ac764c3aa079f1b430
Murilo Fischer <mufischer@hotmail.com>
2024-02-25 23:03:02 -0300

    adicionado comentarios

---
+++ 3-operadores/operators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+ *
+ *
+ *  ref https://www.tutorialspoint.com/cprogramming/c_operators.htm
+ *
+ *   type of operators
+ *   unary, binary and ternary
+ *
+ *   unary ==> + , - --> plus and minus
+ *   unary ==> ++, -- --> increment and decrement
+ *
+ *   binary ==> +,-,/,*,% --> arithmetic operators
+ *   binary ==> =,+=,-=,/=,*=,%= --> assignment operators
+ *   binary ==> ==, !=, <,>,<=,>= --> comparison operators
+ *   binary ==> &&, ||, ! --> logical operators
+ *
+ *   ternary ==> ? : Conditional Operator
+ *
+ *
+ */
+
+int main()
+{
+    return 0;
+}
