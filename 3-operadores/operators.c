5b2234999da8f84d187d9752bbfd465bf7dade20
Murilo Fischer <mufischer@hotmail.com>
2024-02-25 23:21:22 -0300

    operadores

--- 3-operadores/operators.c
+++ 3-operadores/operators.c
@@ -24,5 +24,51 @@
 
 int main()
 {
+    printf("\nForce precedence with()\n");
+    int x = 2 * (3 + 3);
+    int y = (2 * 3) + 3;
+
+    printf("x=%d\n", x);
+    printf("y=%d\n", y);
+
+    printf("Modulus operator: \n");
+    int c = 10 % 3;
+    printf("10 %% 3 = %d\n", c);
+
+    printf("Unary minus:\n");
+    int a = 5;
+    int b = -a;
+
+    printf("b = %d\n", b);
+    printf("a = %d\n", a); // a doesn`t change
+
+    printf("post increment:\n");
+
+    a = 5;
+    b = 5;
+    b = a++;
+
+    printf("b = %d\n", b); // b gets assigned to first
+    printf("a = %d\n", a); // then a is incremented
+
+    b = 5;
+    a = 5; // start fresh
+    b = ++a;
+
+    printf("pre increment:\n");
+    printf("b = %d\n", b); // b gets assigned to second
+    printf("a = %d\n", a); // a is incremented first
+
+    printf("Assignment operators:\n");
+    a = 5;
+    b = 5;
+    b += a;
+
+    printf("b += a = %d\n", b); // 5+5
+    a *= 30;
+    printf("a *= 30 = %d\n", a); // 5*30
+    a %= 140;
+    printf("a %%= 140 = %d\n", a); // 150 % 140
+
     return 0;
 }
