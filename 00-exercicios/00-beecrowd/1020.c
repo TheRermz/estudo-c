f43d6d7f433af62f71fa6b483d7f6945824f5a75
Murilo Fischer <43183795+TheRermz@users.noreply.github.com>
2024-02-27 11:58:00 -0300

    exercicio beecrowd adicionado

---
+++ 00-exercicios/00-beecrowd/1020.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+// Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+
+// Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático simples.
+
+// Entrada
+// O arquivo de entrada contém um valor inteiro.
+
+int main()
+{
+    int ageDays, ageYear, ageMonth, ageDay;
+
+    printf("Digite sua idade em dias:\n");
+    scanf("%d", &ageDays);
+
+    ageYear = ageDays / 365;
+    printf("%d", ageYear);
+
+    return 0;
+}
