266b084c86fb786a680cc2444ca9cdc826c5943c
Murilo <mufischer@hotmail.com>
2024-05-23 10:45:37 -0300

    exercicio união de vetores

--- 00-exercicios/002-einstein/vetores/exercicios/uniao.c
+++ 00-exercicios/002-einstein/vetores/exercicios/uniao.c
@@ -4,26 +4,20 @@
 
 int main()
 {
-    int vetorA[5], vetorB[5], vetorC[5];
+    int vetorA[5], vetorB[5], vetorC[10];
 
     for (int i = 0; i < 5; i++)
     {
         printf("Ola!! Por favor, insira um valor\n");
         scanf("%i", &vetorA[i]);
+        vetorC[i] = vetorA[i];
         printf("Por gentileza, insira mais um valor\n");
         scanf("%i", &vetorB[i]);
-
-        if (vetorA[i] == vetorB[i])
-        {
-            vetorC[i] = vetorA[i];
-        }
+        vetorC[i + 5] = vetorB[i];
     }
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 10; i++)
     {
-        if (vetorC[i] != null)
-        {
-            printf("%i\n", vetorC[i]);
-        }
+        printf("%i", vetorC[i]);
     }
 }
